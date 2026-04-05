fa910efaea334bc8c1358349d08b8d90cd90f342
holechi <kchyun77@gmail.com>
2026-04-05 23:11:01 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,10 +1,12 @@
-const CACHE_NAME = 'info-app-v3'; // 버전을 올려서 강제 업데이트 유도
+const CACHE_NAME = 'info-final-v3';
 const ASSETS = ['index.html', 'manifest.json'];
-
 self.addEventListener('install', (e) => {
   e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
 });
-
+self.addEventListener('activate', (e) => {
+  e.waitUntil(caches.keys().then((keys) => Promise.all(keys.map((k) => k !== CACHE_NAME && caches.delete(k)))));
+});
 self.addEventListener('fetch', (e) => {
   e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
 });
